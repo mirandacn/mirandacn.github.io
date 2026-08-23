574b867b8aae7e71924957b5448562401d94bc1e
CNM <77670462+mirandacn@users.noreply.github.com>
2026-08-23 23:50:36 +0000

    Add Machine Usage Tracker to mirandacn.github.io portfolio

---
+++ config.js
@@ -0,0 +1,9 @@
+// Configuration for Machine Usage Tracker Frontend
+
+// Users list
+const CONFIG = {
+    USERS: ['Luís Sousa', 'Pedro Tavares', 'Claudio Miranda'],
+    MACHINES: ['Gehl', 'Mustang', 'Bobcat', 'Sany'],
+    AUTO_EXPORT_XML: true,
+    SYNC_INTERVAL: 5000
+};
